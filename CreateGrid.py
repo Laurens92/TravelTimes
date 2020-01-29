ab0356652b730071a2d75a4c58cd923f3e45b831
laurens <laurens_vanhorssen@live.nl>
2020-01-29 19:55:33 +0100

    Updated the README on how to install geos/ basemap Updated CreateGrid.py Not working on this anymore, since API key of google is not free of charge anymore. It is now necessary to enable billing

--- CreateGrid.py
+++ CreateGrid.py
@@ -10,17 +10,18 @@ import numpy
 from datetime import datetime
 import pickle
 from func import flat2lla
-from mpl_toolkits.basemap import Basemap
+# For installing basemap: https://matplotlib.org/basemap/users/installing.html
+from mpl_toolkits.basemap import Basemap # Used to check if coordinate is land
 import time
 
 # Input parameters
 destination_address = ""
 
-gmaps = googlemaps.Client(key='PLACE YOUR KEY HERE')
+gmaps = googlemaps.Client(key='')
 
 # Choose rectangular or circular grid (distances + resolution)
 # Choose mode of transport:  "driving", "walking", "bicycling" or "transit"
-TravelMode = "driving" 
+TravelMode = "driving"
 
 # Choose time of arrival, use either now or the Epoch time since January 1, 1970
 TimeOfArrival = 'now'
@@ -56,17 +57,17 @@ map = Basemap(
         urcrnrlon=max(long+1.0),  # Upper Right corner longitude
         urcrnrlat=max(lat+1.0)    # Upper Right corner latitude
         )
-        
+
 # Allocate memory
 TravelTime = numpy.zeros((len(lat),len(long)))
 
-          
+
 k = 0;
 # Compute the travel time around the destination address
-for i in range(0,len(lat)):  
-    for j in range(0,len(long)):  
+for i in range(0,len(lat)):
+    for j in range(0,len(long)):
         # Create lat long coordinates
-        
+
         address = str([lat[i],long[j]])
         address = address[:-1]
         address = address[1:]
@@ -78,38 +79,38 @@ for i in range(0,len(lat)):
             # Request directions via driving
             # Specify arrival time instead of departure time
             # "driving", "walking", "bicycling" or "transit"
-            
+
             #directions_result = gmaps.directions(address,
             #                             destination_address,
             #                             mode=TravelMode,
             #                             arrival_time=TimeOfArrival,
             #                             avoid="ferries")
-            
+
             directions_result = gmaps.directions(address,
-                                         destination_address, 
+                                         destination_address,
                                          mode=TravelMode,
                                          departure_time = TimeOfDeparture,
                                          traffic_model = "best_guess",
                                          #arrival_time=TimeOfArrival,
                                          avoid="ferries")
-            
+
             # Set TravelTime to zero in case the result is empty
             if not directions_result:
                 TravelTime[i][j] = 0
             else:
                 time_dur_s = directions_result[0]['legs'][0]['duration']['value']
                 time_dur_min = time_dur_s/60
-                TravelTime[i][j] = time_dur_min  
+                TravelTime[i][j] = time_dur_min
         else:   # When the coordinates are not at land, set travel time to zero
             TravelTime[i][j] = -50
-              
+
         # print(time_dur_min)
         k = k + 1
         Percentage = "{0:.1f}".format(k/NumberOfPoints*100)
         print(Percentage + ' %')
- 
-    
-    
+
+
+
 # Store the data in a file
 StrName = str(now)
 StrName = StrName.replace('-','')
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # TravelTimes
 Calculates the travel times around a certain location using google maps API
 
-Todo: 
-- Add to github
-- Make code more user friendly
+The following packages are required:
 
-- Testing GIT on linux
+To install basemap & geos on Linux (Ubuntu) use the following the terminal:
+sudo apt-get install libgeos-dev
+sudo pip3 install https://github.com/matplotlib/basemap/archive/master.zip
+
+Quited projected since google api key is not free of charge anymore.
